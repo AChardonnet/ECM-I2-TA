cb572e4547d9432545dcb958195c73000c098d08
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2024-11-18 21:40:44 +0100

    fixed routes

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
     "playground.apps.PlaygroundConfig",
+    "home.apps.HomeConfig",
 ]
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("blog/", include("blog.urls")),
     path("playground/", include("playground.urls")),
+    path("", include("home.urls")),
 ]
